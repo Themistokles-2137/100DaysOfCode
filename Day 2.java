34bb2d178cc31dc236d6d599249a1c18c2ae8dcf
Themistokles-2137 <gg4777@srmist.edu.in>
2024-06-11 09:07:30 +0530

    Add files via upload

---
+++ Day 2.java
@@ -0,0 +1,8 @@
+class Test{
+    public static void main (String []args){
+        int x = 5; //Working with Variables
+        String name = "John";
+        System.out.println(name);
+        System.out.println(x);
+    }
+}
