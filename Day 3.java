fcf5101ac1de4ab91df346f41eb607cb6d993368
Themistokles-2137 <gg4777@srmist.edu.in>
2024-06-12 09:08:34 +0530

    Day 3

---
+++ Day 3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Day3 {
+    public static void main (String []args){
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter name, age and marks:");
+        String name = myObj.nextLine();
+        int age = myObj.nextInt();
+        double marks = myObj.nextDouble();
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+        myObj.close();
+    }
+}
