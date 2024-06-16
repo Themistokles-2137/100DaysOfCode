8c5537496b54a00109bfc2f6abf396fe5dbad2d2
Themistokles-2137 <gg4777@srmist.edu.in>
2024-06-16 16:05:39 +0530

    Day 7

---
+++ Day 7.java
@@ -0,0 +1,14 @@
+class Test{
+    public static void main (String []args){
+        int x = 6;
+        int y = 9;
+        System.out.println(Math.min(x, y));
+        System.out.println(Math.max(x, y));
+        System.out.println(Math.sqrt(y));
+        float z = (float) x;
+        System.out.println(Math.floor(z));
+        System.out.println(Math.ceil(z));
+        double a = (double) y;
+        System.out.println(Math.abs(a));
+    }
+}
