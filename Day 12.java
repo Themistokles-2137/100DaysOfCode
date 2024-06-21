4666616225e84c7eb862d625e9c29b0cad3a929e
Themistokles-2137 <gg4777@srmist.edu.in>
2024-06-21 08:59:18 +0530

    Day 12

---
+++ Day 12.java
@@ -0,0 +1,10 @@
+class Strs{
+    public static void main (String []args){
+        StringBuffer str = new StringBuffer("Hello");
+        str.append(" World!");
+        System.out.println(str);
+        StringBuilder st = new StringBuilder("Hello");
+        st.append(" World!");
+        System.out.println(st);
+    }
+}
