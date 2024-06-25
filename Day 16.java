86d9647a2878b0840eb6e14464fc5460864cdaa0
Themistokles-2137 <gg4777@srmist.edu.in>
2024-06-25 07:38:31 +0530

    Day 16

---
+++ Day 16.java
@@ -0,0 +1,16 @@
+class Main{
+    enum weekdays{
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday    
+    }
+    public static void main(String[] args) {
+        for (weekdays myVar : weekdays.values()){
+            System.out.println(myVar);
+        }
+    }
+}
